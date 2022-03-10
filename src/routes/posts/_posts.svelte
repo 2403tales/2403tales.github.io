bfaefdc6afa1927155b15c164c951038ed3d900f
2403tales <2403tales@protonmail.com>
2022-03-11 03:37:14 +0530

    Minor styling changes

--- src/routes/posts/_posts.svelte
+++ src/routes/posts/_posts.svelte
@@ -16,23 +16,33 @@
   <meta name="description" content={description}>
 </svelte:head>
 
-<div class="container mx-auto">
-  <div class="flex pt-10 pb-10 font-bold">
-    <!-- svelte-ignore a11y-img-redundant-alt -->
-    <div class="mr-2">
-      <a href=#> 
-        <p class="justify-center w-16 leading-tight text-sm text-center">
-          <img class="rounded-full w-16 h-16" src={authorDp} alt="Profile picture"/>
-        </p>
-        <p>{author}</p>
-    </a>
-    </div>
+<div class="container md:mx-auto px-4">
+  <div class="flex pt-5 pb-20 font-bold">
+    
     <div>
+      <div class="mr-2 flex">
+        <div>
+          
+          <!-- svelte-ignore a11y-invalid-attribute -->
+          <a href="#"> 
+            <p class="justify-center w-16 leading-tight text-sm text-center">
+              <!-- svelte-ignore a11y-img-redundant-alt -->
+              <img class="rounded-full w-16 h-16" src={authorDp} alt="Profile picture"/>
+            </p>
+          </a>
+        </div>
+        <div class="ml-3 mb-3">
+          <a href="#"> 
+            <p>{author}</p>
+          </a>
+          <p class="text-gray-400">{date}</p>
+        </div>
+        
+      </div>
       <h1>{title}</h1>
       <div class="flex text-sm mt-2">
-        <p class="text-gray-400 mr-3 mt-1">{date}</p>
         {#each uniqueCategories as category}
-          <p class="p-1 px-1 ml-2 bg-emerald-500 text-black rounded-xl">{category}</p>
+          <p class="p-1 px-1 mr-2 bg-emerald-500 text-black rounded-xl">{category}</p>
         {/each}
       </div>
       
